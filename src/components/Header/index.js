ccb0bc7335350ee8269a0b803c9936c3e98357ce
Gans19 <ganscivil.1999@gmail.com>
2023-03-09 20:12:19 +0530

    loader starts

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,3 +4,4 @@ export { default as Header } from "./Header";
 export { default as MainContainer } from "./mainContainer";
 export { default as CreateContainer } from "./createContainer";
 export { default as HomeContainer } from "../Header/HomeContainer";
+export { default as Loader } from "../Loader";

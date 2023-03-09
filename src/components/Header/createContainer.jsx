a67f6352524ae7d2ba81e4a63fac7ae087676cbd
Gans19 <ganscivil.1999@gmail.com>
2023-03-09 20:09:52 +0530

    starting loader animation

--- src/components/Header/createContainer.jsx
+++ src/components/Header/createContainer.jsx
@@ -18,7 +18,7 @@ const CreateContainer = () => {
 
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center ">
-			<div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center">
+			<div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
 				{fields && (
 					<motion.p
 						initial={{ opacity: 0 }}
@@ -63,6 +63,8 @@ const CreateContainer = () => {
 							))}
 					</select>
 				</div>
+
+				<div className="group flex justify-center flex-col items-center border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg"></div>
 			</div>
 		</div>
 	);

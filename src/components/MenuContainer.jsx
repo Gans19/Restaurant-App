6ad5fc13e2c8681d85c7d20bf9cf6cc74deb60df
Gans19 <ganscivil.1999@gmail.com>
2023-03-20 21:33:05 +0530

    working in filter area

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,25 +1,53 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoFastFood } from "react-icons/io5";
+import { categories } from "../utils/data";
 
 const MenuContainer = () => {
+	const [filter, setFilter] = useState("chicken");
+	useEffect(() => {}, [filter]);
+
 	return (
 		<section className="w-full my-6 " id="menu">
-			<div className="w-full flex flex-col items-center justify-center">
+			<div className=" w-full flex flex-col items-center justify-center">
 				<p className="mr-auto text-2xl font-semibold capitalize relative text-headingColor before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
 					Our Fresh And Healthy Foods
 				</p>
 
-				<div className="w-full flex items-center justify-start lg:justify-center gap-8 overflow-x-scroll scrollbar-none">
-					<div className="group bg-card hover:bg-red-600 w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center duration-150 transition-all ease-in-out">
-						<div className="w-10 h-10 rounded-full bg-red-600 group-hover:bg-card flex items-center justify-center">
-							<IoFastFood className="text-card group-hover:text-textColor text-lg" />
-						</div>
-						<p className="text-sm text-textColor group-hover:text-white">
-							Category
-						</p>
-					</div>
+				<div className="my-6 w-full flex items-center justify-start lg:justify-center gap-8 overflow-x-scroll scrollbar-none">
+					{categories &&
+						categories.map((category) => (
+							<div
+								onClick={() => setFilter(category.urlParamName)}
+								key={category.id}
+								className={`group ${
+									filter === category.urlParamName ? "bg-cartNumBg" : "bg-card"
+								} hover:bg-red-600 w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center duration-150 transition-all ease-in-out`}>
+								<div
+									className={`w-10 h-10 rounded-full shadow-lg ${
+										filter === category.urlParamName
+											? "bg-white"
+											: "bg-cartNumBg"
+									} group-hover:bg-card flex items-center justify-center`}>
+									<IoFastFood
+										className={` ${
+											filter === category.urlParamName
+												? "text-textColor"
+												: "text-card"
+										} group-hover:text-textColor text-lg`}
+									/>
+								</div>
+								<p
+									className={`text-sm ${
+										filter === category.urlParamName
+											? "text-white"
+											: "text-textColor"
+									}  group-hover:text-white`}>
+									{category.name}
+								</p>
+							</div>
+						))}
 				</div>
 			</div>
 		</section>
